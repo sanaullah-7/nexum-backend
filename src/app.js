40b5def5583fe585f074dd4f2f952d4170b1522b
Anees ur Rehman <91630318+Anees201173@users.noreply.github.com>
2026-05-10 23:52:05 +0500

    changes dones

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 const corsOrigin = process.env.CORS_ORIGIN;
 const defaultCorsOrigins = [
   "https://nexum-erp-frontend.vercel.app",
+  "https://erp.nexumtechnologies.com",
   "http://localhost:5173",
   "http://localhost:3000",
 ];
